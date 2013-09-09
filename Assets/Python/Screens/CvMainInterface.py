4ae279539ab8854776bed7942f2530d942e6bd6e
Anquietas <martin.lauer.ap@gmail.com>
2013-09-09 16:36:10 +0100

    Super Spies Code Removed

--- Assets/Python/Screens/CvMainInterface.py
+++ Assets/Python/Screens/CvMainInterface.py
@@ -4988,13 +4988,7 @@ class CvMainInterface:
 																				
 
 												
-# Super Spies Content - Start
-													
-												#if (gc.getTeam(gc.getGame().getActiveTeam()).getCounterespionageTurnsLeftAgainstTeam(eTeam) != 0):
-													#szTempBuffer = u"(%d)%c" %(gc.getTeam(gc.getGame().getActiveTeam()).getCounterespionageTurnsLeftAgainstTeam(eTeam), CyGame().getSymbolID(FontSymbols.OCCUPATION_CHAR))
-													#szBuffer = szBuffer + szTempBuffer
-												
-# Super Spies Content - End											
+
 												bEspionageCanSeeResearch = False
 												if (bEspionage):
 													for iMissionLoop in range(gc.getNumEspionageMissionInfos()):
