70b4acb5194d133997f461de2d0bda2789979094
Warren <xorchiser@gmail.com>
2013-11-13 19:59:12 +0000

    New Civics + Hidden Contract Killing
    Added several new civics (some are entirely new, some are just renamed
    initial starting civics) and hid contract killing (and the associated
    units/building) until it gets a rework

--- Assets/Python/Screens/CvCivicsScreen.py
+++ Assets/Python/Screens/CvCivicsScreen.py
@@ -35,8 +35,8 @@ class CvCivicsScreen:
 		self.HEADINGS_BOTTOM = 280
 		self.HELP_TOP = 350
 		self.HELP_BOTTOM = 610
-		self.TEXT_MARGIN = 13
-		self.BUTTON_SIZE = 24
+		self.TEXT_MARGIN = 12
+		self.BUTTON_SIZE = 22
 		self.BIG_BUTTON_SIZE = 64
 		self.BOTTOM_LINE_TOP = 630
 		self.BOTTOM_LINE_WIDTH = 1014
